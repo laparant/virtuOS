103214f43032de2ff77efc9cb847773cd839fc0f
pgaulier <paul.gaulier@free.fr>
2017-04-04 17:17:42 +0200

    starting implementation !

--- src/thread.c
+++ src/thread.c
@@ -1,1 +1,33 @@
 #include "thread.h"
+#include "queue.h"
+
+typedef struct thread
+{
+    struct ucontext* ctx;
+
+} thread;
+
+thread_t thread_self(void)
+{
+
+}
+
+int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg)
+{
+
+}
+
+int thread_yield(void)
+{
+
+}
+
+int thread_join(thread_t thread, void **retval)
+{
+
+}
+
+void thread_exit(void *retval)
+{
+
+}
--- src/thread.h
+++ src/thread.h
@@ -8,7 +8,7 @@
  *     mais attention aux inconvénient des tableaux de threads
  *     (consommation mémoire, cout d'allocation, ...).
  */
-typedef void * thread_t;
+typedef void* thread_t;
 
 /* recuperer l'identifiant du thread courant.
  */
