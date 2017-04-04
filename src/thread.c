2ea7952cefb237d558a7247013df3d7ef3e369e1
pgaulier <paul.gaulier@free.fr>
2017-04-04 18:06:47 +0200

    added basic thread structure and global variables

--- src/thread.c
+++ src/thread.c
@@ -1,19 +1,27 @@
+#include <stdlib.h>
+#include <ucontext.h>
+
 #include "thread.h"
 #include "queue.h"
 
 typedef struct thread
 {
-    struct ucontext* ctx;
-
+    thread *sleepq;
+    void *data; /* We need to choose between data and counter */
+    struct ucontext *ctx;
 } thread;
+
+static thread *g_current_thread;
+static thread *g_all_threads;
 
 thread_t thread_self(void)
 {
-
+    return current_thread;
 }
 
 int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg)
 {
+    all_threads = realloc(all_threads, sizeof(all_threads) + sizeof(thread));
 
 }
 
