0140d4179559b856488d32c6a0fa1e9a230755ef
MOURET Louise <lmouret001@caillasse.pedago.ipb.fr>
2017-04-10 11:36:47 +0200

    tests inc_counter and dec_counter

--- tst/test_retval.c
+++ tst/test_retval.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define ITER 20
+
 int tst__init_retval() {
-  struct retval *rv = init_retval();
-  if(rv != NULL) {
+  struct retval *rv = NULL;
+  rv = init_retval();
+  if((rv != NULL) && (rv->counter == 0)) {
     free(rv); //free value if value is m'allocated
     return EXIT_SUCCESS;
   }
@@ -15,8 +18,38 @@ int tst__init_retval() {
 
 //TODO find a test for free
 
+int tst__inc_counter() {
+  struct retval * rv = init_retval();
+  int i = 1;
+  for(; i <= ITER ; ++i) {
+    inc_counter(rv);
+    if(rv->counter != i) {
+      free_retval(rv);
+      return EXIT_FAILURE;
+    }
+  }
+  free_retval(rv);
+  return EXIT_SUCCESS;
+}
+
+int tst__dec_counter() {
+  struct retval * rv = init_retval();
+  int i = ITER;
+  rv->counter = ITER + 1;
+  for(; i >= 0 ; --i) {
+    dec_counter(rv);
+    if(rv->counter != i) {
+      free_retval(rv);
+      return EXIT_FAILURE;
+    }
+  }
+  return EXIT_SUCCESS;
+}
+
 int main() {
   assert((tst__init_retval() == EXIT_SUCCESS) && "test init_retval failed\n");
+  assert((tst__inc_counter() == EXIT_SUCCESS) && "test inc_counter failed\n");
+  assert((tst__dec_counter() == EXIT_SUCCESS) && "test dec_counter failed\n");
 
   return 0;
 }
