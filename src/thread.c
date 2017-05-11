6c262f96e250f895f1ea399e56dc23516d4f4bca
lparant <lparant@enseirb-matmeca.fr>
2017-05-11 16:54:48 +0200

    refactoring a bit the thread.c code

--- src/thread.c
+++ src/thread.c
@@ -12,6 +12,11 @@
 
 #define CHECK(val, errval, msg) if ((val) == (errval)) {perror(msg); exit(EXIT_FAILURE);}
 
+/*
+ * ##############################################################################################
+ * ######                                Structures                                        ######
+ * ##############################################################################################
+ */
 typedef struct thread_base thread_base;
 
 typedef struct thread
@@ -22,44 +27,46 @@ typedef struct thread
     STAILQ_ENTRY(thread) to_free_entries; // This entry will be used for the to_free queue
 } thread;
 
-/* The thread that is currently being executed */
-static thread * g_current_thread;
-
-/* The list of all the threads that were created */
-static STAILQ_HEAD(thread_list_all, thread) g_all_threads;
-
-/* The run queue */
-static STAILQ_HEAD(thread_list_run, thread) g_runq;
-
-/* The to free queue */
-static STAILQ_HEAD(thread_list_free, thread) g_to_free;
-
 typedef struct thread_base
 {
-    thread *sleepq;
+    thread *joinq;
     struct retval *rv;
     ucontext_t *ctx;
     int valgrind_stackid;
     int status;
 } thread_base;
 
-thread_t thread_self(void)
-{
-    return g_current_thread;
-}
+/*
+ * ______________________________________________________________________________________________
+ */
 
+/*
+ * ##############################################################################################
+ * ######                                Static datas                                      ######
+ * ##############################################################################################
+ */
 
-void force_exit(void *(*func)(void *), void *funcarg)
-{
-    if(func != NULL)
-    {
-        void *res = func(funcarg);
-        thread_exit(res);
-    }
-}
+// The thread that is currently being executed
+static thread * g_current_thread;
 
+// The list of all the threads that were created
+static STAILQ_HEAD(thread_list_all, thread) g_all_threads;
 
-/* Clean the process exiting */
+// The run queue
+static STAILQ_HEAD(thread_list_run, thread) g_runq;
+
+// The to free queue
+static STAILQ_HEAD(thread_list_free, thread) g_to_free;
+
+/*
+ * ______________________________________________________________________________________________
+ */
+
+/*
+ * ##############################################################################################
+ * ######                             Cleaning processes                                   ######
+ * ##############################################################################################
+ */
 void free_context(thread *th)
 {
     STAILQ_REMOVE(&g_to_free, th, thread, to_free_entries);
@@ -79,15 +86,56 @@ void free_join(thread *th)
     free(th);
 }
 
-int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg)
+/*
+ * ______________________________________________________________________________________________
+ */
+
+/*
+ * ##############################################################################################
+ * ######                            Auxiliary processes                                   ######
+ * ##############################################################################################
+ */
+
+void force_exit(void *(*func)(void *), void *funcarg)
 {
-    /* Initialization of the context */
+    if(func != NULL)
+    {
+        void *res = func(funcarg);
+        thread_exit(res);
+    }
+}
+
+void finalize_join(thread *th, void ** retval)
+{
+    /* Collecting the value of retval */
+    if (retval) *retval = get_value(th->addr->rv);
+
+    /* If not thread main free the resources */
+    if(th != STAILQ_FIRST(&g_all_threads)) {
+      // Removing the thread from the threads joignable
+      STAILQ_REMOVE(&g_all_threads, th, thread, all_entries);
+      free_join(th);
+    }
+}
+
+/*
+ * ______________________________________________________________________________________________
+ */
+
+/*
+ * ##############################################################################################
+ * ######                         Initialisation processes                                 ######
+ * ##############################################################################################
+ */
+
+thread * init_context(void *(*func)(void *), void *funcarg)
+{
     thread *th = malloc(sizeof(thread));
-    CHECK(th, NULL, "thread_create: thread pointer malloc")
+    CHECK(th, NULL, "init_context: thread pointer malloc")
     th->addr = malloc(sizeof(thread_base));
-    CHECK(th->addr, NULL, "thread_create: thread_base malloc")
+    CHECK(th->addr, NULL, "init_context: thread_base malloc")
     th->addr->ctx = malloc(sizeof(ucontext_t));
-    CHECK(th->addr->ctx, NULL, "thread_create: context malloc")
+    CHECK(th->addr->ctx, NULL, "init_context: context malloc")
     getcontext(th->addr->ctx);
 
     th->addr->ctx->uc_stack.ss_size = 64 * 1024;
@@ -99,48 +147,30 @@ int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg)
     th->addr->ctx->uc_link = NULL;
     makecontext(th->addr->ctx, (void (*)(void)) force_exit, 2, func, funcarg);
 
-    /* Get the return value */
-    th->addr->rv = init_retval();
+    return th;
+}
 
-    /* Initialize the thread's sleep queue */
-    th->addr->sleepq = NULL;
-
+void add_to_scheduler(thread * th)
+{
     /* Insert the current thread in the run queue */
     STAILQ_INSERT_TAIL(&g_runq, th, runq_entries);
     /* Put the thread in g_all_threads so we can free it later */
     STAILQ_INSERT_TAIL(&g_all_threads, th, all_entries);
-    *newthread = (thread_t) th;
-
-    return EXIT_SUCCESS;
 }
 
-int thread_yield(void)
+/*
+ * ______________________________________________________________________________________________
+ */
+
+/*
+ * ##############################################################################################
+ * ######                         Errors testing functions                                 ######
+ * ##############################################################################################
+ */
+
+int is_existing(thread * th)
 {
-    STAILQ_INSERT_TAIL(&g_runq, g_current_thread, runq_entries);
-    thread *new_current = STAILQ_FIRST(&g_runq);
-
-    STAILQ_REMOVE_HEAD(&g_runq, runq_entries);
-
-    thread *tmp = g_current_thread;
-    g_current_thread = new_current;
-    CHECK(swapcontext(tmp->addr->ctx, new_current->addr->ctx), -1, "thread_yield: swapcontext")
-
     struct thread *th_i;
-    STAILQ_FOREACH(th_i, &g_to_free, to_free_entries)
-    {
-        free_context(th_i);
-    }
-
-    return EXIT_SUCCESS;
-}
-
-int thread_join(thread_t thread, void **retval)
-{
-    /* Joining the threads waiting for the end of the thread */
-    struct thread *th = (struct thread *) thread;
-
-    /* Search if the thread does exist => error : ESRCH */
-    /*struct thread *th_i;
     STAILQ_FOREACH(th_i, &g_all_threads, all_entries)
     {
         if(th_i == th)
@@ -148,50 +178,101 @@ int thread_join(thread_t thread, void **retval)
         if(STAILQ_NEXT(th_i,all_entries) == NULL)
             return ESRCH;
     }
-    */
+
+    return EXIT_SUCCESS;
+}
+
+/*
+ * ______________________________________________________________________________________________
+ */
+
+/*
+ * ##############################################################################################
+ * ######                             Thread functions                                     ######
+ * ##############################################################################################
+ */
+
+thread_t thread_self(void)
+{
+    return g_current_thread;
+}
+
+int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg)
+{
+    /* Initialization of the context */
+    thread * th = init_context(func, funcarg);
+
+    /* Initialization of the return value */
+    th->addr->rv = init_retval();
+
+    /* Initialize the thread's sleep queue */
+    th->addr->joinq = NULL;
+
+    /* Add the thread to the scheduler */
+    add_to_scheduler(th);
+
+    /* Giving the return value */
+    *newthread = (thread_t) th;
+
+    return EXIT_SUCCESS;
+}
+
+int thread_yield(void)
+{
+    /* Free the processes waiting to be freed */
+    struct thread *th_i;
+    STAILQ_FOREACH(th_i, &g_to_free, to_free_entries)
+    {
+        free_context(th_i);
+    }
+
+    /* Update scheduler */
+    STAILQ_INSERT_TAIL(&g_runq, g_current_thread, runq_entries);
+    thread *new_current = STAILQ_FIRST(&g_runq);
+    STAILQ_REMOVE_HEAD(&g_runq, runq_entries);
+
+    /* Swapping contexes */
+    thread *tmp = g_current_thread;
+    g_current_thread = new_current;
+    CHECK(swapcontext(tmp->addr->ctx, new_current->addr->ctx), -1, "thread_yield: swapcontext")
+
+    return EXIT_SUCCESS;
+}
+
+int thread_join(thread_t thread, void **retval)
+{
+    struct thread *th = (struct thread *) thread;
+
+    /* Search if the thread does exist => error : ESRCH */
+    // is_existing(th);
+
     /* Detecting the deadlock (the thread is waiting for me) => error : EDEADLK */
     struct thread *me = thread_self();
-    if (th == me->addr->sleepq)
+    if (th == me->addr->joinq)
         return EDEADLK;
 
     /* Detecting if the thread is already joined by another thread */
-    if (th->addr->sleepq != NULL)
+    if (th->addr->joinq != NULL)
         return EINVAL;
 
-    /* If the thread has already finished: an error might occur but we won't check it */
+    /* If the thread has already finished */
     if (th->addr->status != RUNNING)
     {
-        if (retval) *retval = get_value(th->addr->rv);
-        /* If not thread main free the resources */
-        if(th != STAILQ_FIRST(&g_all_threads)) {
-          // Removing the thread from the threads joignable
-          STAILQ_REMOVE(&g_all_threads, th, thread, all_entries);
-          free_join(th);
-        }
+        finalize_join(th, retval);
         return EXIT_SUCCESS;
     }
 
     /* If the thread is alive */
-    th->addr->sleepq = me;
-
     /* Sleeping while the thread hasn't finished */
+    th->addr->joinq = me;
     struct thread *new_current = STAILQ_FIRST(&g_runq);
-
     STAILQ_REMOVE_HEAD(&g_runq, runq_entries);
-
     struct thread *tmp = g_current_thread;
     g_current_thread = new_current;
     CHECK(swapcontext(tmp->addr->ctx, new_current->addr->ctx), -1, "thread_join: swapcontext")
 
-    /* Collecting the value of retval */
-    if (retval) *retval = get_value(th->addr->rv);
-
-    /* If not thread main free the resources */
-    if(th != STAILQ_FIRST(&g_all_threads)) {
-      // Removing the thread from the threads joignable
-      STAILQ_REMOVE(&g_all_threads, th, thread, all_entries);
-      free_join(th);
-    }
+    /* When woke up (thread is finished) */
+    finalize_join(th, retval);
     return EXIT_SUCCESS;
 }
 
@@ -204,8 +285,8 @@ __attribute__ ((__noreturn__)) void thread_exit(void *retval)
     if (retval) me->addr->rv->value = retval;
 
     /* Waking up the thread waiting for me */
-    if(me->addr->sleepq != NULL)
-        STAILQ_INSERT_TAIL(&g_runq, me->addr->sleepq, runq_entries);
+    if(me->addr->joinq != NULL)
+        STAILQ_INSERT_TAIL(&g_runq, me->addr->joinq, runq_entries);
 
     /* Yielding to next thread if others threads are running*/
     if (!STAILQ_EMPTY(&g_runq))
@@ -234,44 +315,34 @@ __attribute__ ((__noreturn__)) void thread_exit(void *retval)
     exit(EXIT_SUCCESS);
 }
 
-/*#############################################################################################*/
+/*
+ * ______________________________________________________________________________________________
+ */
+
+/*
+ * ##############################################################################################
+ * ######                            Specific main functions                               ######
+ * ##############################################################################################
+ */
 
 __attribute__ ((constructor)) void thread_create_main (void)
 {
-    /* Initialization of the current thread */
     /* Initialization of the context */
-    thread *th = malloc(sizeof(thread));
-    CHECK(th, NULL, "thread_create: thread pointer malloc")
-    th->addr = malloc(sizeof(thread_base));
-    CHECK(th->addr, NULL, "thread_create: thread_base malloc")
-    th->addr->ctx = malloc(sizeof(ucontext_t));
-    CHECK(th->addr->ctx, NULL, "thread_create: context malloc")
-    getcontext(th->addr->ctx);
+    thread *th = init_context(NULL, NULL);
 
-    th->addr->ctx->uc_stack.ss_size = 64 * 1024;
-    th->addr->ctx->uc_stack.ss_sp = malloc(th->addr->ctx->uc_stack.ss_size);
-    int valgrind_stackid = VALGRIND_STACK_REGISTER(th->addr->ctx->uc_stack.ss_sp,
-                                                   th->addr->ctx->uc_stack.ss_sp + th->addr->ctx->uc_stack.ss_size);
-    th->addr->valgrind_stackid=valgrind_stackid;
-    th->addr->status = RUNNING;
-    th->addr->ctx->uc_link = NULL;
-    makecontext(th->addr->ctx, (void (*)(void)) force_exit, 2, NULL, NULL);
-
-    /* Get the return value */
+    /* Initialization of the return value */
     th->addr->rv = init_retval();
 
     /* Initialize the thread's sleep queue */
-    th->addr->sleepq=NULL;
-
-    /* Initializing the current thread */
-    g_current_thread = th;
+    th->addr->joinq = NULL;
 
     /* Initialization of the queues */
     STAILQ_INIT(&g_all_threads);
     STAILQ_INIT(&g_runq);
     STAILQ_INIT(&g_to_free);
 
-    /* Insert in the global queue */
+    /* Add the thread to the scheduler */
+    g_current_thread = th;
     STAILQ_INSERT_HEAD(&g_all_threads, th, all_entries);
 }
 
@@ -289,8 +360,8 @@ __attribute__ ((destructor)) void thread_exit_main (void)
         // could not be done because no retval : find a solution
 
         /* Waking up the thread waiting for me */
-        if(me->addr->sleepq != NULL)
-            STAILQ_INSERT_TAIL(&g_runq, me->addr->sleepq, runq_entries);
+        if(me->addr->joinq != NULL)
+            STAILQ_INSERT_TAIL(&g_runq, me->addr->joinq, runq_entries);
 
         /* If others threads are running */
         while (!STAILQ_EMPTY(&g_runq))
@@ -312,13 +383,14 @@ __attribute__ ((destructor)) void thread_exit_main (void)
     th = STAILQ_FIRST(&g_to_free);
     while(th != NULL)
     {
-        th2 = STAILQ_NEXT(th, all_entries);
+        th2 = STAILQ_NEXT(th, to_free_entries);
         if (th != main_thread)
         {
             free_context(th);
         }
         th = th2;
     }
+    STAILQ_INIT(&g_to_free);
 
 
     /* Free the remaining things */
@@ -342,5 +414,9 @@ __attribute__ ((destructor)) void thread_exit_main (void)
 
     STAILQ_INIT(&g_all_threads);
 }
+
+/*
+ * ______________________________________________________________________________________________
+ */
 
 #endif
